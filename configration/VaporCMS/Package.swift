29ce9ba700057fa785ac327e7d70e87bd8037020
rb_de0 <rebirth.de0@gmail.com>
2016-09-21 18:29:46 +0900

    マニフェストの更新

--- configration/VaporCMS/Package.swift
+++ configration/VaporCMS/Package.swift
@@ -4,10 +4,9 @@ let package = Package(
     name: "VaporCMS",
     
     dependencies: [
-        .Package(url: "https://github.com/qutheory/vapor.git", versions: Version(0,12,0)..<Version(0,12,1)),
-        .Package(url: "https://github.com/qutheory/vapor-mustache.git", majorVersion: 0, minor: 8),
-        .Package(url: "https://github.com/novi/mysql-swift.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/rb-de0/SwiftyMarkdownParser.git", majorVersion: 0, minor: 1)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/novi/mysql-swift.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/rb-de0/SwiftyMarkdownParser.git", majorVersion: 0, minor: 2)
     ],
     exclude: [
         "Config",
