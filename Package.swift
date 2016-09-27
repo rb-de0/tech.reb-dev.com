d4f19ee1e3883c195850e27a6d3d6376563da427
rb_de0 <rebirth.de0@gmail.com>
2016-09-27 11:39:36 +0900

    ディレクトリ構成の変更

--- Package.swift
+++ Package.swift
@@ -2,7 +2,6 @@ import PackageDescription
 
 let package = Package(
     name: "VaporCMS",
-    
     dependencies: [
         .Package(url: "https://github.com/rb-de0/vapor.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/novi/mysql-swift.git", majorVersion: 0, minor: 5),
