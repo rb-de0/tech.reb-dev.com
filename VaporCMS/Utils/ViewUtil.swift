37ea648277c10ba27035db57aefe5fe9d7b20b61
rb_de0 <rebirth.de0@gmail.com>
2016-09-28 17:08:06 +0900

    リンク機能追加（てきとーすぎる・・・）

--- VaporCMS/Utils/ViewUtil.swift
+++ VaporCMS/Utils/ViewUtil.swift
@@ -1,12 +1,35 @@
 import Vapor
 import HTTP
 import Node
+import Settings
 
 class ViewUtil{
+
+    private static var linkList = [Node]()
+
+    class func configure(config: Settings.Config){
+        guard let linkArray = config["link", "linklist"]?.array else{
+            return
+        }
+
+        let linkObjects = linkArray.flatMap{$0.object}
+
+        linkList = linkObjects.map{(dictionary: [String: Polymorphic]) -> Node in
+            var dic = [String: Node]()
+            
+            for (key, value) in dictionary{
+                dic[key] = Node(value.string ?? "")
+            }
+
+            return Node(dic)
+        }
+    }
+
     class func contextIncludeHeader(request: Request, context: [String: Node]) -> Node{
         let subContentNames = SubContentAccessor.loadAll().map{$0.name}.map{Node($0)}
 
         var includedContext = context
+        includedContext["links"] = Node(linkList)
         includedContext["subcontentnames"] = Node(subContentNames)
         includedContext["has_session"] = Node(SessionManager.hasSession(request: request))
         return Node(includedContext)
