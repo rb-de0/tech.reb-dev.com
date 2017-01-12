7984e316d2cc8a0f8bce9351481ff993c49ac117
rb_de0 <rebirth.de0@gmail.com>
2017-01-12 17:55:58 +0900

    日付取得ロジックの修正

--- Sources/App/Utils/DateUtil.swift
+++ Sources/App/Utils/DateUtil.swift
@@ -3,9 +3,17 @@ import Foundation
 class DateUtil{
     
     class func currentDate() -> String {
+        
+        let now: Date
+        if let interval = TimeZone(abbreviation: "JST")?.secondsFromGMT(){
+            now = Date(timeIntervalSinceNow: TimeInterval(interval))
+        } else {
+            now = Date()
+        }
+        
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ja_JP")
         formatter.dateFormat = "yyyy年M月dd日 HH時mm分"
-        return formatter.string(from: Date())
+        return formatter.string(from: now)
     }
 }
