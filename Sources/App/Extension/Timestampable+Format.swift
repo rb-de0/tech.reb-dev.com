9336e2a6be61e3f373d7e49b8edf3492f22248ea
rb_de0 <rebirth.de0@gmail.com>
2017-07-18 22:17:39 +0900

    Fix time zone

--- Sources/App/Extension/Timestampable+Format.swift
+++ Sources/App/Extension/Timestampable+Format.swift
@@ -6,6 +6,7 @@ extension Timestampable {
     var formattedCreatedAt: String? {
 
         let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(secondsFromGMT: 60 * 60 * 9)
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         
         return createdAt.map { formatter.string(from: $0) }
@@ -14,6 +15,7 @@ extension Timestampable {
     var formattedUpdatedAt: String? {
         
         let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(secondsFromGMT: 60 * 60 * 9)
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         
         return updatedAt.map { formatter.string(from: $0) }
